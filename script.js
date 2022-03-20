ad588c904c87340e0fb60177e56980522ccf00a6
Flavie Parmentier <flavie.parmentier@gmail.com>
2022-03-21 00:19:18 +0100

    JS onclick

---
+++ script.js
@@ -0,0 +1,4 @@
+const accessConfig = document.getElementById("accessconfig");
+const click = () => accessConfig.addEventListener(click, (event) => {
+  window.open('https://accessconfig.a11y.fr/', '_blank');
+})
