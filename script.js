8ceb7e227d15b9abe347f9f7460c273a5f2f6291
Flavie Parmentier <flavie.parmentier@gmail.com>
2022-03-21 01:32:37 +0100

    last changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Ecouteur au click sur bouton accessibilité
+// Renvoie vers un onglet informatif (au 2e click -> à corriger)
 const accessConfig = document.getElementById("accessconfig");
 const clicker = () => accessConfig.addEventListener("click", (event) => {
   window.open('https://accessconfig.a11y.fr/', '_blank');
