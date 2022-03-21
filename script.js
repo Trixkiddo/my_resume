90b70070439c7b2781173e2b3c37e1ac99d4cfb8
Flavie Parmentier <flavie.parmentier@gmail.com>
2022-03-21 16:35:21 +0100

    details

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const accessConfig = document.getElementById("accessconfig");
 const clicker = () => accessConfig.addEventListener("click", (event) => {
   window.open('https://accessconfig.a11y.fr/', '_blank');
 });
+
+//form
+const emailField = document.getElementById("emailField");
+const button = document.getElementById("button2");
+const response = document.getElementById("response");
+
+button.addEventListener("click", function () {
+  const email = emailField.value;
+  if (validateEmail(email)) {
+    response.innerHTML = "Hiya Cowboy, this email will work for us 🤠";
+  } else {
+    response.innerHTML = "Sorry, this email is not cool enough 😩";
+  }
+});
+
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
